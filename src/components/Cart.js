28379328e2fada706049f80880b72f8e612560d8
seungyeon Lee <asfire0302@gmail.com>
2022-04-18 19:16:53 +0900

    :sparkles: 장바구니 수량 증가, 감소 버튼 추가

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,38 +1,76 @@
 import React from 'react';
+import { useState } from 'react';
 import {Table} from 'react-bootstrap'
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
+let CngBtn = styled.button`
+  padding: 2px;
+  margin: 3px;
+  width: 30px
+`
 
+let CloseBtn = styled.button`
+  border-radius:5px;
+  margin-top: 10px;
+`
 
 export function Cart(props){
+
+  let state = useSelector((state)=> state.reducer)
+  let [alert,alertCng] = useState(true)
+  
+  let dispatch = useDispatch()
+
+  const alertClose = ()=>{
+    alertCng(false)
+  }
   return(
     <>
     <Table responsive>
-  <thead>
-    <tr>
-      <th>상품명</th>
-      <th>가격</th>
-      <th>수량</th>
-      <th>변경</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>{props.state[0].name}</td>
-      <td>{props.state[0].price}</td>
-      <td>{props.state[0].quan}</td>
-      <td></td>
-    </tr>
-  </tbody>
-</Table>
+      <thead>
+        <tr>
+          <th>상품명</th>
+          <th>가격</th>
+          <th>수량</th>
+          <th>변경</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          state.map((item,idx)=>{
+            return(
+              <tr key={idx}>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.quan}</td>
+                <td><CngBtn className="btn btn-danger" onClick={()=>{ dispatch({ type : 'quanPlus'}) }}>+</CngBtn></td>
+                <td><CngBtn className="btn btn-danger" onClick={()=>{ dispatch({ type : 'quanMinus'}) }}>-</CngBtn></td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+  </Table>
+  { alert === true
+    ?(
+      <div className='my-alert-yellow'>
+          <p>🎉 지금 구매하시면 타임 세일 20%</p>
+          <CloseBtn className="btn btn-danger" onClick={()=>{alertClose()}}>닫기</CloseBtn>
+      </div>
+    )
+    : null
+  }
     </>
   )
 }
 
-function getStore(state){
+/* /* function getStore(state){
   return{
-    state : state
+    state : state.reducer
   }
-}//redux store 데이터 가져와서 props로 변환해주는 함수(state를 props화)
+}//redux store 데이터 가져와서 props로 변환해주는 함수(state를 props화) -> useSelecotor로 대체*/
 
-export default connect(getStore)(Cart)
+export default Cart
