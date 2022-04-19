902dd7b9814a17d8a9e84cadca6de6b5db7706a3
seungyeon Lee <asfire0302@gmail.com>
2022-04-19 22:44:57 +0900

    :sparkles: 상세페이지로 이동 기능 추가

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import { stockContext } from '../App'
 
 export default function Product(props){
   let stock = useContext(stockContext)
+  let history = useHistory()
   return(
     <>
-      <div className='col-md-4'>
+      <div className='col-md-4' onClick={()=>{history.push('/detail/' + props.shoes.id)}}>
               <img src={'http://codingapple1.github.io/shop/shoes' + (props.idx+1) + '.jpg'} width="100%" alt="상품이미지" />
               <h4>{props.shoes.title}</h4>
               <p>{props.shoes.content}</p>
               <p>₩ {props.shoes.price}</p>
-              <p>재고 : {stock[props.idx]}</p>
       </div>
 
       
